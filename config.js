da371154a51e6164df2c35dd6067fa457f6c76ce
David Bzura <bzuradavid@gmail.com>
2020-06-08 20:53:14 -0300

    variables entorno

---
+++ config.js
@@ -0,0 +1,12 @@
+require ( 'dotenv' ).config()
+
+module.export = {
+    database: {
+        host: process.env.DB_HOST || 'localhost',
+        port: process.env.PORT || '3306',
+        user: process.env.USER || 'root',
+        pass: process.env.PASS || 'root',
+        db_name: process.env.DB_NAME || 'delilah',
+        dialect: process.env.DIALECT || 'mysql'
+    }
+}
--- db_connect.js
+++ db_connect.js
@@ -1,6 +1,8 @@
+const env = require( 'config' );
 
+const { host, port, user, pass, db_name, dialect } = env.database;
 
 const Sequelize = require( 'sequelize' );
-const sequelize = new Sequelize( 'delilah', 'root', 'root', { dialect: 'mysql' } );
+const sequelize = new Sequelize( db_name, user, pass, { dialect: dialect } );
 
 module.exports = sequelize;
