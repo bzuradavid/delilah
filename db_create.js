8d4461cd2623022ad19fb6745c510023b82c245c
David Bzura <bzuradavid@gmail.com>
2020-06-15 16:52:28 -0300

    fix en script creacion db

--- db_create.js
+++ db_create.js
@@ -14,7 +14,7 @@ sequelize.authenticate().then(async () => {
     }
     query = "CREATE DATABASE delilah";
     result = await sequelize.query(query, { raw: true });
-    query = "CREATE TABLE delilah.orders ( order_id INT PRIMARY KEY AUTO_INCREMENT , user_id INT NOT NULL , status_id INT DEFAULT '0' NOT NULL , payment_method_id DEFAULT '0' INT NOT NULL , created_date DATETIME NOT NULL )";
+    query = "CREATE TABLE delilah.orders ( order_id INT PRIMARY KEY AUTO_INCREMENT, user_id INT NOT NULL , status_id INT NOT NULL DEFAULT '0' , payment_method_id INT NOT NULL DEFAULT '0' , created_date DATETIME NOT NULL )";
     result = await sequelize.query(query, { raw: true });
     query = "CREATE TABLE delilah.users ( user_id INT PRIMARY KEY AUTO_INCREMENT , full_name VARCHAR(50) NOT NULL , email VARCHAR(100) NOT NULL , phone VARCHAR(100) NOT NULL , full_address VARCHAR(150) NOT NULL , password VARCHAR(20) NOT NULL , role VARCHAR(20) NOT NULL )";
     result = await sequelize.query(query, { raw: true });
@@ -25,5 +25,7 @@ sequelize.authenticate().then(async () => {
     query = "CREATE TABLE delilah.statuses ( status_id INT PRIMARY KEY AUTO_INCREMENT , title VARCHAR(50) )"; 
     result = await sequelize.query(query, { raw: true });
     query = "CREATE TABLE delilah.payment_methods ( payment_method_id INT PRIMARY KEY AUTO_INCREMENT , title VARCHAR(50) )";
+    result = await sequelize.query(query, { raw: true });
+    query = "INSERT INTO delilah.users (full_name, email, phone, full_address, password, role) VALUES ( 'Super admin', 'admin@delilah.com', '3515555555', 'Av. Siempreviva 576', '12345678', 'admin' )";
     result = await sequelize.query(query, { raw: true });
 });
