52f67c2bc50b3408d32ba02d6f1b56329c6d0fd9
David Bzura <bzuradavid@gmail.com>
2020-06-16 20:09:59 -0300

    environment variables fixed

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ module.exports= {
     database: {
         host: process.env.DB_HOST || 'localhost',
         port: process.env.PORT || '3306',
-        user: process.env.USER || 'root',
-        pass: process.env.PASS || 'root',
+        user: process.env.DB_USER || 'root',
+        pass: process.env.DB_PASS || 'root',
         db_name: process.env.DB_NAME || 'delilah',
         dialect: process.env.DIALECT || 'mysql'
     }
